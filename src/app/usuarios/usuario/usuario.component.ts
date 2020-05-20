be64747189b1a3a5eebfacede42da2fab6be22d3
Angel Hinostroza Calderon <RP1416@realplaza.local>
2020-05-19 22:37:19 -0500

    effects terminado

--- src/app/usuarios/usuario/usuario.component.ts
+++ src/app/usuarios/usuario/usuario.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.reducers';
+import { cargarUsuario } from 'src/app/store/actions';
+import { Usuario } from '../../models/usuario.model';
 
 @Component({
   selector: 'app-usuario',
@@ -7,10 +12,25 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class UsuarioComponent implements OnInit {
+  
+  usuario: Usuario;
+  loading: boolean = false;
+  error: any;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private store: Store<AppState>
+  ) { }
 
   ngOnInit(): void {
+    this.store.select('usuario').subscribe(({ user, loading, error }) => {
+      this.usuario = user;
+      this.loading = loading;
+      this.error = error;
+    })
+    this.route.params.subscribe(({ id }) => {
+      this.store.dispatch(cargarUsuario({ id }));
+    })
   }
 
 }
